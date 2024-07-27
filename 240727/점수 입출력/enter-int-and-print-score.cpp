740277ffe9783aa227d7574b329e22708957ad7c
박지호 <57872222k@gmail.com>
2024-07-27 18:46:30 +0900

    Title: 점수 입출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240727/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int s;
+
+	cin >> s;
+
+	cout << "Your score is " << s << " point.";
+    
+    return 0;
+}
