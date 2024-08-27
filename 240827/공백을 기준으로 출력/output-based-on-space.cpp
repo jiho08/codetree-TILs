6433cea5a4ec950d0b71e1b12c49206a697521c9
박지호 <57872222k@gmail.com>
2024-08-27 18:14:01 +0900

    Title: 공백을 기준으로 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240827/공백을 기준으로 출력/output-based-on-space.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+int main()
+{
+	string a, b;
+
+	getline(cin, a);
+	getline(cin, b);
+
+	a.erase(find(a.begin(), a.end(), ' '));
+	b.erase(find(b.begin(), b.end(), ' '));
+
+	cout << a << b;
+
+	return 0;
+}
